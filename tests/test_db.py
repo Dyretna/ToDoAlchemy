7d06459b172f988db3364c1c685007a06150fc57
Jesper Anteryd <jesper.anteryd@gmail.com>
2025-06-20 16:55:45 +0200

    Apply code style fixes via pre-commit (black, ruff, eof-fixer)

--- tests/test_db.py
+++ tests/test_db.py
@@ -8,23 +8,21 @@ def test_get_close_db(app):
     with app.app_context():
         db = get_db()
         assert db is get_db()
-        
+
     with pytest.raises(sqlite3.ProgrammingError) as e:
-        db.execute('SELECT 1')
-        
-    assert 'closed' in str(e.value)
-    
-    
+        db.execute("SELECT 1")
+
+    assert "closed" in str(e.value)
+
+
 def test_init_db_command(runner, monkeypatch):
     class Recorder(object):
         called = False
-        
+
     def fake_init_db():
         Recorder.called = True
-        
-    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
-    result = runner.invoke(args=['init-db'])
-    assert 'initialized the database.\n' in result.output
+
+    monkeypatch.setattr("flaskr.db.init_db", fake_init_db)
+    result = runner.invoke(args=["init-db"])
+    assert "initialized the database.\n" in result.output
     assert Recorder.called
-    
-    
